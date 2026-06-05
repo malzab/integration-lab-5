2235f345dc72b6d57e32eb65775f05aca365107a
Malwina Zabielska <malwinazabielska1@gmail.com>
2026-06-05 16:44:31 +0200

    Dodanie nasłuchiwania portu w index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
+const express = require('express');
+const app = express();
+
+app.get("/", (req, res) => {
+  res.status(200).send("OK");
+});
+
+const PORT = process.env.PORT || 3000
+
+app.listen(PORT, () => {
+  console.log(`Server działa na porcie ${PORT}`);
+});
+
 function sum(a,b){
     return a+b;
 }
